668a1037b5a28ce6f7fbc6984971d88ef6715920
Charles Tyra <charlestyra@MacBook-Pro.local>
2023-01-04 15:07:54 -0500

    transpose

--- skeleton/phase_1_arrays.js
+++ skeleton/phase_1_arrays.js
@@ -29,8 +29,21 @@ console.log([-1, 0, 1, 0, 2, 2].twosum())
 
 Array.prototype.transpose = function() {
     let uniqArr = []
+    let colArr = []
 
-    
-
-    return uniqArr
+   for(col = 0; col < this[0].length; col++){
+      colArr = []
+      for(row = 0; row < this.length; row++){
+         colArr.push(this[row][col]);
+      }
+      uniqArr.push(colArr);
+   }
+   return uniqArr;
 }
+
+
+console.log([
+   [1, 1, 1],
+   [2, 2, 2],
+   [3, 3, 3]].transpose()
+);
