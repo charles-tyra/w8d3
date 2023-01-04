30fac533ea50f802f53f31f0650c19d7a0e52cfe
Charles Tyra <charlestyra@MacBook-Pro.local>
2023-01-04 17:17:49 -0500

    this is so confusing

--- skeleton/phase_2_enumerable.js
+++ skeleton/phase_2_enumerable.js
@@ -1,15 +1,27 @@
 Array.prototype.myEach = function(cb){
-  
     for (i = 0; i < this.length; i++) {
         cb(this[i]);
     }
-    //return undefined;
 }
 
 function callback(el) {
-   console.log(el + '!');
+   return el + '!';
 }
 
-
 // let arr = [1, 2, 3, 4];
-[1, 2, 3, 4].myEach(callback);
+console.log([1, 2, 3, 4].myEach(callback));
+
+Array.prototype.myMap = function(cb) {
+   const freeArr = [];
+
+   let funk = (el) => freeArr.push(cb(el));
+
+   this.myEach(funk);
+   return freeArr;
+}
+
+const closure = [1,2,3,4,5].myMap();
+
+console.log(closure(callback));
+
+
