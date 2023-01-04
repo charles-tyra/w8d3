50c070e544fb2ec9949db6cc4f097e7327c039a9
App Academy Student <email@email.com>
2023-01-04 14:53:58 -0500

    twosum

--- skeleton/phase_1_arrays.js
+++ skeleton/phase_1_arrays.js
@@ -10,3 +10,18 @@ Array.prototype.uniq = function() {
 }
 
 console.log([1, 2, 2, 3, 3, 3].uniq())
+
+Array.prototype.twosum = function() {
+    let uniqArr = [];
+
+    for(i = 0; i < this.length - 1; i++){
+        for(j = i + 1; j < this.length; j++){
+            if(this[i] + this[j] === 0) {
+                uniqArr.push([i, j])
+            }
+        }
+    }
+    return uniqArr
+}
+
+console.log([-1, 0, 1, 0, 2, 2].twosum())
