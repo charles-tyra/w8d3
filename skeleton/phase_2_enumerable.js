e1ce58c6f9f8f315938aad34353e51ab1496a8e9
Charles Tyra <charlestyra@MacBook-Pro.local>
2023-01-04 16:20:27 -0500

    changed ordering of myEach and callback, fixes issue due to hoisting?

--- skeleton/phase_2_enumerable.js
+++ skeleton/phase_2_enumerable.js
@@ -1,7 +1,3 @@
-function callback(el) {
-    console.log(el + '!' );
-}
-
 Array.prototype.myEach = function(cb){
   
     for (i = 0; i < this.length; i++) {
@@ -10,6 +6,10 @@ Array.prototype.myEach = function(cb){
     //return undefined;
 }
 
-let arr = [1, 2, 3, 4];
-arr.myEach(callback);
+function callback(el) {
+   console.log(el + '!');
+}
 
+
+// let arr = [1, 2, 3, 4];
+[1, 2, 3, 4].myEach(callback);
